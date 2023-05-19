bb51de307df0e018e654d17a3693d63ea47e0b9b
Viper <462534624@qq.com>
2023-05-19 11:57:31 +0800

    简化功能

--- src/main/java/io/github/oneincase/config/KettleAutoConfiguration.java
+++ src/main/java/io/github/oneincase/config/KettleAutoConfiguration.java
@@ -1,7 +1,5 @@
 package io.github.oneincase.config;
 
-import io.github.oneincase.service.DataBaseService;
-import io.github.oneincase.service.impl.DataBaseServiceImpl;
 import io.github.oneincase.core.KettleMain;
 import io.github.oneincase.core.KettleProperties;
 import org.pentaho.di.core.exception.KettleException;
@@ -23,15 +21,6 @@ public class KettleAutoConfiguration {
         KettleMain kettleMain = new KettleMain(kettleProperties);
         kettleMain.run(); // 启动kettle
         return kettleMain;
-    }
-
-    /**
-     * 注入dataBaseService 方便调用
-     */
-    @Bean
-    @ConditionalOnBean(KettleMain.class)
-    public DataBaseService dataBaseService(){
-        return new DataBaseServiceImpl();
     }
 
 }
